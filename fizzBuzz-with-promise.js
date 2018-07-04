ff82e8ce97a455f66b0d8f8249d537564b08bd81
jsc215 <jeffcomertennis@gmail.com>
2018-07-04 08:29:20 -0400

    quick update

--- fizzBuzz-with-promise.js
+++ fizzBuzz-with-promise.js
@@ -7,17 +7,21 @@ function timeout(ms) {
 }
 
 async function fizzBuzz() {
-  for (let i = 1; i <= 100; i++) {
-    await timeout(1000);
-    if (i % 5 == 0 && i % 3 == 0) {
-      console.log('fizzBuzz');
-    } else if (i % 3 == 0) {
-      console.log('fizz');
-    } else if (i % 5 == 0) {
-      console.log('buzz');
-    } else {
-      console.log(i);
+  try {
+    for (let i = 1; i <= 100; i++) {
+      await timeout(1000);
+      if (i % 5 == 0 && i % 3 == 0) {
+        console.log('fizzBuzz');
+      } else if (i % 3 == 0) {
+        console.log('fizz');
+      } else if (i % 5 == 0) {
+        console.log('buzz');
+      } else {
+        console.log(i);
+      }
     }
+  } catch (error) {
+    console.error(error)
   }
 }
 
